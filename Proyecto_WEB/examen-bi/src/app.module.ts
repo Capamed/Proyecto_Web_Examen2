e0888a266423dcf5a1895295493b6f4bc28a3968
jonathan01.07.95 <jonathan01.07.95@gmail.com>
2019-02-01 22:28:25 -0500

    agregacion app module

--- Proyecto_WEB/examen-bi/src/app.module.ts
+++ Proyecto_WEB/examen-bi/src/app.module.ts
@@ -8,6 +8,14 @@ import {RolPorUsuarioEntity} from "./rol-por-usuario/rol-por-usuario.entity";
 import {UsuarioModule} from "./usuario/usuario.module";
 import {RolPorUsuarioModule} from "./rol-por-usuario/rol-por-usuario.module";
 import {RolModule} from "./rol/rol.module";
+import { AutorModule } from './autor/autor.module';
+import { LibroModule } from './libro/libro.module';
+import { EventoModule } from './evento/evento.module';
+import { EventoLibroModule } from './evento-libro/evento.module';
+import { AutorEntity } from './autor/autor.entity';
+import { LibroEntity } from './libro/libro.entity';
+import { EventoEntity } from './evento/evento.entity';
+import { EventoLibroEntity } from './evento-libro/evento.entity';
 
 
 @Module({
@@ -26,11 +34,19 @@ import {RolModule} from "./rol/rol.module";
               RolEntity,
               UsuarioEntity,
               RolPorUsuarioEntity,
+              AutorEntity,
+              LibroEntity,
+              EventoEntity,
+              EventoLibroEntity
           ],
       }),
       UsuarioModule,
       RolPorUsuarioModule,
       RolModule,
+      AutorModule,
+      LibroModule,
+      EventoModule,
+      EventoLibroModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- Proyecto_WEB/examen-bi/src/evento-libro/evento.module.ts
+++ Proyecto_WEB/examen-bi/src/evento-libro/evento.module.ts
@@ -1,14 +1,17 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { EventoLibroEntity } from "./evento.entity";
+import { EventoLibroService } from "./evento.service";
+import { EventoLibroController } from "./evento.controller";
 
 
 @Module({
     imports: [TypeOrmModule.forFeature([
-        
+        EventoLibroEntity
     ])],
-    providers: [],
-    controllers: [],
-    exports: [],
+    providers: [EventoLibroService],
+    controllers: [EventoLibroController],
+    exports: [EventoLibroService],
 })
 
 export class EventoLibroModule {
